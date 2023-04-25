1d5841c59a4c3512aaaecdcce9111be67f3cd652
Remics89 <Remics89@gmail.com>
2023-04-24 17:50:42 -0700

    feat: parent component starting blocks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import React from 'react';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 
+const App = () => {
+
+
+    return (
+        <div className="app">Personal Site</div>
+    )
+}
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
