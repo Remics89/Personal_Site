5b137fa895438f07507fdb7028c3ec0f76303764
Remics89 <Remics89@gmail.com>
2023-04-30 02:20:46 -0700

    feat: foundational starting components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Fragment } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./footer";
 
 const App = () => {
-
-
     return (
-        <div className="main">Personal Site</div>
-    )
-}
+        <Fragment>
+            <div className="header">
+                <div className="title">Matthew Szekeres</div>
+                <div className="title2">Junior Web Developer</div>
+            </div>
+            <div className="display">
+                <img className="ui fluid image" src="/images/wireframe/image.png" />
+            </div>
+            <div className="main">
+                <h3 class="ui header">Section One</h3>
+                <div class="ui section divider"></div>
+                <h3 class="ui header">Section Two</h3>
+            </div>
+            <Footer />
+        </Fragment>
+    );
+};
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(
     <BrowserRouter>
-        <App/>
+        <App />
     </BrowserRouter>
-)
+);
