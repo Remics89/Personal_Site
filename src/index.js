d895edc8c7250f117cf67d4bf52e3941c0353961
Remics89 <Remics89@gmail.com>
2023-05-18 17:23:24 -0700

    feat: image transition call, image state, content sections

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./footer";
+import Transition from "./imageTransition";
+
+import fittrk from "./images/FITTRK.png";
+import retro from "./images/Retro.png";
 
 const App = () => {
+
+    const [imgindex, setimgindex] = useState(0);
+
     return (
         <Fragment>
             <div className="header">
                 <div className="title">Matthew Szekeres</div>
                 <div className="title2">Junior Web Developer</div>
             </div>
-            <div className="display">
-                <img className="ui fluid image" src="/images/wireframe/image.png" />
-            </div>
+            <Transition imgindex={imgindex} setimgindex={setimgindex} />
             <div className="main">
-                <h3 class="ui header">Section One</h3>
-                <div class="ui section divider"></div>
-                <h3 class="ui header">Section Two</h3>
+                <h3 className="ui header">Section One</h3>
+                <div className="ui section divider"></div>
+                <h3 className="ui header">Section Two</h3>
             </div>
             <Footer />
         </Fragment>
